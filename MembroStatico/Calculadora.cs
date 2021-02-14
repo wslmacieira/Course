aecc1fbc92bcdeceb02a05047993e7b2ef7a5737
wslmacieiradev <wslmacieira@outlook.com>
2021-02-14 17:54:07 -0300

    adiciona classe Calculadora

---
+++ MembroStatico/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MembroStatico
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        public static double VolumeCircunferencia(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3); // mesma coisa que r * r * r
+        }
+    }
+}
--- MembroStatico/Program.cs
+++ MembroStatico/Program.cs
@@ -5,30 +5,20 @@ namespace MembroStatico
 {
     class Program
     {
-
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
             Console.Write("Entre com o valor do raio: ");
             double raio =  double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
+            double circ = Calculadora.Circunferencia(raio);
 
-            double volume = VolumeCircunferencia(raio);
+            double volume = Calculadora.VolumeCircunferencia(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
 
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-
-        static double VolumeCircunferencia(double r)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(r, 3); // mesma coisa que r * r * r
-        }
+     
     }
 }
