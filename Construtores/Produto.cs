10e1eb15a12736779eab417f454f004884b75f1a
wslmacieiradev <wslmacieira@outlook.com>
2021-02-15 10:05:06 -0300

    sobrecarga

---
+++ Construtores/Produto.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+
+namespace Construtores
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto() // construtor padrão
+        {
+
+        }
+        public Produto (string nome, double preco, int quantidade) // com 3 argumentos
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto (string nome, double preco) // com 2 argumentos
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 10;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+         public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
