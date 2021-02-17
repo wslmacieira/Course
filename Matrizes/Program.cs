e47433acf60849c5aa8a9d3e99b7ce0191e387f5
wslmacieiradev <wslmacieira@outlook.com>
2021-02-17 11:38:02 -0300

    matrizes

---
+++ Matrizes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+
+            Console.WriteLine(mat.Length);// quantos elementos a matriz tem no total
+
+            Console.WriteLine(mat.Rank);// quanto e a quantidade de linhas
+
+            Console.WriteLine(mat.GetLength(0));//primeira dimensão da matriz linhas
+            Console.WriteLine(mat.GetLength(1));//primeira dimensão da matriz colunas
+        }
+    }
+}
