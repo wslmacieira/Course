7a7f0c962602201d81f1470c2053e2848a59a382
wslmacieiradev <wslmacieira@outlook.com>
2021-02-17 10:34:34 -0300

    listas parte 1

---
+++ Lista/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>(); // cria uma lista vazia
+
+            List<string> list2 = new List<string> { "Maria", "Alex" };
+        }
+    }
+}
