a80e962c00da2640e5071851fed7b820d5da0d46
wslmacieiradev <wslmacieira@outlook.com>
2021-02-17 09:54:26 -0300

    modificador de parametros params

---
+++ ModificadorParams/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace ModificadorParams
+{
+    class Calculator
+    {
+       // public static int Sum(int[] numbers)
+        public static int Sum(params int[] numbers) // params
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ ModificadorParams/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ModificadorParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3 ); // com params não precisa instanciar
+            int s2 = Calculator.Sum(new int[] { 2, 3, 4 }); // mais verboso
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
