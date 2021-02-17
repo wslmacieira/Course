8a83c6aeac7cb30d440c35031acf3200271f94bb
wslmacieiradev <wslmacieira@outlook.com>
2021-02-16 22:31:10 -0300

    cria class point

---
+++ TipoRerefenciaTipoValor/Point.cs
@@ -0,0 +1,12 @@
+﻿namespace TipoRerefenciaTipoValor
+{
+    struct Point
+    {
+        public double X, Y;
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+    }
+}
---
+++ TipoRerefenciaTipoValor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TipoRerefenciaTipoValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variavel apenas declarada inicia em estado não atribuida e o proprio
+            //compilador não permite que ela seja acessada.
+            Point p; // mesmo sendo struct precisa inicializar
+
+            p.X = 10;
+            p.Y = 20;
+
+            Console.WriteLine(p);
+            
+            p = new Point(); // recria as caixas com valo 0
+
+            Console.WriteLine(p);
+        }
+    }
+}
