8b149de023741d8d80b067465c887642dc295579
wslmacieiradev <wslmacieira@outlook.com>
2021-02-16 22:47:54 -0300

    nullable

--- TipoRerefenciaTipoValor/Program.cs
+++ TipoRerefenciaTipoValor/Program.cs
@@ -8,16 +8,52 @@ namespace TipoRerefenciaTipoValor
         {
             //variavel apenas declarada inicia em estado não atribuida e o proprio
             //compilador não permite que ela seja acessada.
-            Point p; // mesmo sendo struct precisa inicializar
+            /*   Point p; // mesmo sendo struct precisa inicializar
 
-            p.X = 10;
-            p.Y = 20;
+               p.X = 10;
+               p.Y = 20;
 
-            Console.WriteLine(p);
-            
-            p = new Point(); // recria as caixas com valo 0
+               Console.WriteLine(p);
 
-            Console.WriteLine(p);
+               p = new Point(); // recria as caixas com valo 0
+
+               Console.WriteLine(p);
+            */
+
+            //Nullable
+
+            //double x = null; // tipos structs não aceitam null
+
+           /* double? x = null; //Nullable<double> x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+           */
+
+            // Operador de coalescência nula
+
+            double? x = null;
+            double? y = 10;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
     }
 }
